dd63da08f28f420d893df08a381d9f26f4ed7087
Kolya Venturi <kolya.venturi@enx3s.com>
2019-01-21 16:40:37 -0700

    Error / info logging first pass

--- src/index.js
+++ src/index.js
@@ -1,9 +1,27 @@
 // @flow
 
+import {log} from './logger';
+
 export default class Logger {
   scope: string | null;
+  scopeTag: string;
 
   constructor(scope?: string) {
     this.scope = scope || null;
+    this.scopeTag = scope ? `[${scope}] ` : '';
+  }
+
+  log = (data: any, type: ?string) => {
+    log(data, {type});
+  }
+
+  logInfo(info) {
+    const {scopeTag} = this;
+    log(`${scopeTag}${info}`);
+  }
+
+  logError(error) {
+    const {scopeTag} = this;
+    this.log(`${scopeTag}${error}`, 'error');
   }
 }
--- test/index.test.js
+++ test/index.test.js
@@ -1,5 +1,18 @@
 import test from 'ava';
+import {spy} from 'sinon';
 import Logger from '../src';
+
+let logStub;
+let errorStub;
+test.before(() => {
+  logStub = spy(console, 'log');
+  errorStub = spy(console, 'error');
+});
+
+test.after.always(() => {
+  logStub.restore();
+  errorStub.restore();
+});
 
 test('Can create a logger with generic scope', t => {
   const logger = new Logger();
@@ -12,4 +25,44 @@ test('Can create a logger with a specified scope', t => {
   const logger = new Logger(scope);
 
   t.is(logger.scope, scope);
+});
+
+test('Logs without scope', t => {
+  const logger = new Logger();
+  const msg = 'some info';
+
+  logger.logInfo(msg);
+
+  t.true(logStub.calledWith(msg));
+});
+
+test('Logs with scope', t => {
+  const scope = 'scope';
+  const logger = new Logger(scope);
+  const msg = 'some info';
+
+  logger.logInfo(msg);
+
+  const expected= `[${scope}] ${msg}`;
+  t.true(logStub.calledWith(expected));
+});
+
+test('Errors without scope', t => {
+  const logger = new Logger();
+  const msg = 'some info';
+
+  logger.logError(msg);
+
+  t.true(errorStub.calledWith(msg));
+});
+
+test('Errors with scope', t => {
+  const scope = 'scope';
+  const logger = new Logger(scope);
+  const msg = 'some info';
+
+  logger.logError(msg);
+
+  const expected= `[${scope}] ${msg}`;
+  t.true(errorStub.calledWith(expected));
 });
