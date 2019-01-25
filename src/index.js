75841adc4cd01ff2cc53db81e350efe3ac4f626e
Kolya Venturi <kolya.venturi@enx3s.com>
2019-01-24 21:52:04 -0700

    Add toggle for colors

--- src/index.js
+++ src/index.js
@@ -9,7 +9,8 @@ type DatabaseType = {
 
 type ArgType = {
   scope?: string,
-  database?: DatabaseType
+  database?: DatabaseType,
+  color?: boolean
 };
 
 const noop = () => {};
@@ -19,17 +20,20 @@ export default class Logger {
 
   database: DatabaseType;
 
-  constructor({scope, database}: ArgType = {}) {
+  color: boolean;
+
+  constructor({scope, database, color}: ArgType = {}) {
     this.scope = scope || null;
     this.database = database || {save: noop};
+    this.color = !(color === false);
   }
 
   log = (data: any, args: {[string]: any} = {}) => {
-    const {scope, database} = this;
+    const {scope, database, color} = this;
     let {type, ...extraArgs} = args;
     type = type || types.INFO;
 
-    const logResult = log(data, {type, scope, ...extraArgs});
+    const logResult = log(data, {type, scope, color, ...extraArgs});
 
     if (typeof database.save === 'function') {
       database.save(logResult);
